0620b5653ad5ed5f7bd03ab29f06ba7fddcd18cb
819165391 <819165391@VLELI0106.animaedu.intranet>
2020-03-04 22:29:48 -0300

    Atualização

---
+++ projtos/src/projtos/Aula1.java
@@ -0,0 +1,15 @@
+package projtos;
+
+public class Aula1 {
+
+	public Aula1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Olá mundo!");	
+			System.out.println("Hello world!");
+	}
+
+}
